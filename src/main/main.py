6d27b3ee368ed8fe6ce1bebbe371a27f09a679f9
Bandura2 <119165162+Bandura2@users.noreply.github.com>
2023-10-20 21:57:02 +0300

    Add third algo lab

--- src/main/main.py
+++ src/main/main.py
@@ -1,17 +1,74 @@
-def calculate_width_board(n, w, h):
+class BinaryTree:
 
-    if n < 1 or w < 1 or h < 1 or n >= 1012 or w >= 109 or h >= 109 or w == h:
-        return -1, -1
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
 
-    left = 1
-    right = max(w, h) * n
 
-    while left != right:
-        temp = (left + right) // 2
+def is_tree_balanced(node: BinaryTree) -> bool:
+    return tree_height(node) != -1
 
-        if ((temp // w) * (temp // h)) >= n:
-            right = temp
-        else:
-            left = temp + 1
 
-    return left
+def tree_height(current_node: BinaryTree):
+    if current_node is None:
+        return 0
+
+    left_subtree_height = tree_height(current_node.left)
+    if left_subtree_height == -1:
+        return -1
+
+    right_subtree_height = tree_height(current_node.right)
+    if right_subtree_height == -1:
+        return -1
+
+    if abs(left_subtree_height - right_subtree_height) > 1:
+        return -1
+
+    return max(left_subtree_height, right_subtree_height) + 1
+
+
+# def is_tree_balanced(node: BinaryTree):
+#
+#     if node.left is None and node.right is None:
+#         return True
+#     elif node.left is None or node.right is None:
+#         return False
+#     else:
+#         is_left_tree_balanced = is_tree_balanced(node.left)
+#         is_right_tree_balanced = is_tree_balanced(node.right)
+#
+#     if is_left_tree_balanced and is_right_tree_balanced:
+#         return True
+#     elif is_left_tree_balanced or is_right_tree_balanced:
+#         return False
+#     else:
+#         return False
+
+
+# left_root = BinaryTree(1)
+# left_root.left = BinaryTree(0)
+# left_root.right = BinaryTree(2)
+#
+# right_root = BinaryTree(5)
+# right_root.left = BinaryTree(4)
+# right_root.right = BinaryTree(6)
+# right_root.right.right = BinaryTree(7)
+#
+# main_root = BinaryTree(3)
+# main_root.left = left_root
+# main_root.right = right_root
+#
+# print(f"Is binary tree balanced: {is_tree_balanced(main_root)}")
+#
+#
+# def print_binary_tree(node: BinaryTree):
+#     if node.left is not None:
+#         print_binary_tree(node.left)
+#
+#     print(f"Elem {node.value}")
+#     if node.right is not None:
+#         print_binary_tree(node.right)
+#
+#
+# print_binary_tree(main_root)
